87bcf9819b627293df983c00e0303e18f0fdc7c8
rickyrichard007 <therickyrichard@gmail.com>
2022-04-15 23:13:27 +0530

    pre mint start date

--- backend/src/config.js
+++ backend/src/config.js
@@ -72,7 +72,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-04-30T13:30:48+05:30"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-15T13:30:48+05:30"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 300; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x53F2A328817d56a008034b69A2347cde4689713B"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
